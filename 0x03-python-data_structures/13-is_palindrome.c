d785ce44a96c52def7e33315c7640d5465b4cbbf
Boye <adeboyeakingbelure18@yahoo.com>
2022-06-06 06:52:39 -0700

    p

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,22 +11,22 @@ int is_palindrome(listint_t **head)
 	listint_t *start = NULL, *end = NULL;
 	unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
 
-	 if (head == NULL)
-		 return (0); 
-	 if (*head == NULL)
-		 return (1);
-	 start = *head;
-	 len = listint_len(start);
-	 len_cyc = len * 2;
-	 len_list = len_cyc - 2;
-	 end = *head;
-	 for (; i < len_cyc; i = i + 2)
-	 {
-		 if (start[i].n != end[len_list].n)
-			 return (0);
-		 len_list = len_list - 2;
-	 }
-	 return (1);
+	if (head == NULL)
+		return (0);
+	if (*head == NULL)
+		return (1);
+	start = *head;
+	len = listint_len(start);
+	len_cyc = len * 2;
+	len_list = len_cyc - 2;
+	end = *head;
+	for (; i < len_cyc; i = i + 2)
+	{
+		if (start[i].n != end[len_list].n)
+			return (0);
+		len_list = len_list - 2;
+	}
+	return (1);
 }
 /**
  * get_nodeint_at_index - Gets a node from a linked list
