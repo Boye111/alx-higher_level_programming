f4d68ef1b75072987f3406e6c687094a168625c4
Boye <adeboyeakingbelure18@yahoo.com>
2022-06-08 08:33:26 -0700

    done

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is  None:
+        return None
+    highest = max(a_dictionary.values(), default=None)
+    for a, b in a_dictionary.items():
+        if b == highest:
+            return a
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin.python3
+def multiply_by_2(a_dictionary):
+    another_dictionary = a_dictionary.copy()
+
+    for k, v in another_dictionary.items():
+        another_dictionary[k] = v * 2
+    return another_dictionary
