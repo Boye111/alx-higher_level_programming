62481c41d736b30632694b7eeb08d92e55be5cbe
Boye <adeboyeakingbelure18@yahoo.com>
2022-06-10 02:50:35 -0700

    just

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,12 +4,12 @@ def roman_to_int(roman_string):
         return 0
     roman_letters = [
             ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
-            ['X', 10], ['V', 5], ['I', 1]
+           i ['X', 10], ['V', 5], ['I', 1]
             ]
     num = 0
     last = 0
 
-    for letters in roman_string:
+    for letter in roman_string:
         for elem in roman_letters:
             if letter == elem[0]:
                 if last == 0 or last >= elem[1]:
