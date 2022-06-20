fa9343d09792b28048e4aaa0b91a3e995a83197b
Boye <adeboyeakingbelure18@yahoo.com>
2022-06-20 05:40:44 -0700

    iono

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
             new_list.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             new_list.append(0)
-            print('division by zero')
+            print('division by 0')
             continue
         except TypeError:
             new_list.append(0)
