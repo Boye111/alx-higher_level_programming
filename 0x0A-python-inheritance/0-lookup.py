a8e22c460ec23d91c411aabc1e8bcaf882bb53f4
Boye <adeboyeakingbelure18@yahoo.com>
2022-07-04 13:17:42 -0700

    python inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""
+Returns the list of available attributes
+"""
+
+
 def lookup(obj):
-    """
-    Returns the list of available attributes
-    """
-
-
+    """ Function to do nothing """
     return dir(obj)
