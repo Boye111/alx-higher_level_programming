f4180ff13f1e75eb9a662ecaa4268bb38aee8f9e
Boye <adeboyeakingbelure18@yahoo.com>
2022-07-04 13:28:13 -0700

    PI

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,6 +5,7 @@ A function that return True or False
 
 
 def inherits_from(obj, a_class):
+    """ inherits from some """
     if isinstance(obj, a_class) and \
             issubclass(a_class, obj.__class__) is False:
                 return True
