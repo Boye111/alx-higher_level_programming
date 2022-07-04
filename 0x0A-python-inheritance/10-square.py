8a8bf3a2bda5eee72d25f1c12140f22903187c7e
Boye <adeboyeakingbelure18@yahoo.com>
2022-07-04 13:11:42 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+A square with inheritance from a rectangle
+"""
+
+
+class Square(Rectangle):
+    """ A square with the rectangle """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
