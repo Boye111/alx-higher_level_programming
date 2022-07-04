40c105329687b700d8272b17b9169ffdf7d4493a
Boye <adeboyeakingbelure18@yahoo.com>
2022-07-04 13:35:42 -0700

    pi

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,5 +8,6 @@ def inherits_from(obj, a_class):
     """ inherits from some """
     if isinstance(obj, a_class) and \
             issubclass(a_class, obj.__class__) is False:
-                return True
+        return True
+
     return False
