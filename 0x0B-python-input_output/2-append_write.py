e0055f3f3068638344fbae6e80b9f028b07d1d21
Boye <adeboyeakingbelure18@yahoo.com>
2022-07-05 11:56:10 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,5 +6,5 @@ Append to a file
 
 def append_write(filename="", text=""):
     """a function that appends a string at the end of a text file"""
-    with open(filename, mode='a' encoding='utf-8') as f:
+    with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
