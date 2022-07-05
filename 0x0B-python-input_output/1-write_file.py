d419682daf2d1af3b1eefaef317d8d2b644dfd8f
Boye <adeboyeakingbelure18@yahoo.com>
2022-07-05 09:39:42 -0700

    pycode

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ Write to a file
 
 def write_file(filename="", text=""):
     """function that writes a string to a text file"""
-    with open(filename, encoding='utf-8') as f:
-        return len(f.readlines())
+    with open(filename, mode='w' encoding='utf-8') as f:
+        return f.write(text)
