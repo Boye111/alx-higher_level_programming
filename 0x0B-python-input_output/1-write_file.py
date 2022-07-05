66f04f7f8c107e98c897eeffe7a7f28b7da05d3a
Boye <adeboyeakingbelure18@yahoo.com>
2022-07-05 11:54:03 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,5 @@ Write to a filebto a text string
 
 def write_file(filename="", text=""):
     """ Function that writes a string to a text file """
-
-    with open(filename, mode='w' encoding='utf-8') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)
