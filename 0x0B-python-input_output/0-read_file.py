f60de225fc53a092bcd65a178a6d4ff8dd5dc375
Boye <adeboyeakingbelure18@yahoo.com>
2022-07-05 04:36:56 -0700

    0X0B

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Documentation about 0x0B
+"""
+
+
 def read_file(filename=""):
+    """Task 1 of the above operation"""
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
