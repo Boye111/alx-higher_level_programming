6c5526cac381f8380ddaf383dc70bc8bbe1303e1
Boye <adeboyeakingbelure18@yahoo.com>
2022-09-06 15:42:47 -0700

    javascript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 const len = process.argv.length;
 const nums = process.argv.slice(2).map(function (n) {
-	return parseInt(n);
+  return parseInt(n);
 });
 const max = Math.max.apply(Math, nums);
 const min = Math.min.apply(Math, nums);
 
 if (len > 3) {
-	let i = 0;
-	let n = 0;
-	let secbig = min;
+  let i = 0;
+  let n = 0;
+  let secbig = min;
 
-	for (; i < len; ++i) {
-		n = nums[i];
+  for (; i < len; ++i) {
+    n = nums[i];
 
-		if (n > secbig && n < max) {
-			secbig = n;
-		}
-	}
-	console.log(secbig);
+    if (n > secbig && n < max) {
+      secbig = n;
+    }
+  }
+  console.log(secbig);
 } else {
-	console.log(0);
+  console.log(0);
 }
