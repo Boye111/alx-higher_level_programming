35af875823ba72566f91bac5d44d87a258eab818
Boye <adeboyeakingbelure18@yahoo.com>
2022-09-07 07:56:52 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor (w, h) {
-    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
-      this.w = width;
-      this.h = heigth;
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
     }
   }
 };
