c87cd8cdd9cb1cef853893ddb21e9678ace356a8
Boye <adeboyeakingbelure18@yahoo.com>
2022-09-14 07:22:14 -0700

    orm

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     """ all cities in a state """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          password=argv[2], db=argv[3])
-    with db.cursor as cur:
+    with db.cursor() as cur:
         cur.execute("""
             SELECT
                 cities.id, cities.name
