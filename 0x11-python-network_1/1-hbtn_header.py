f5fbabbc612f74d33acd8138d3d73bb512ca3e3d
Boye <adeboyeakingbelure18@yahoo.com>
2022-09-25 22:35:31 -0700

    network

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-sends request to get content found in the header
+request to get content found in the header displays value of the X-Request-Id
 """
 
 
@@ -8,7 +8,7 @@ from sys import argv
 from urllib.request import Request, urlopen
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     request = Request(argv[1])
 
     with urlopen(request) as res:
